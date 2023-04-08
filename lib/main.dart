3686e7d8212dcb2ece0412ceb8124a3d4ac8f6d8
Lucas Muniz Lara <lucasml@live.com>
2023-04-08 13:21:44 +0044

    adiciona dependencia para formatação de datas

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(const ExpensesApp());
 
@@ -73,7 +74,7 @@ class MyHomePage extends StatelessWidget {
                                 style: TextStyle(fontWeight: FontWeight.bold),
                               ),
                               Text(
-                                e.date.toString(),
+                                DateFormat('d MMM y').format(e.date),
                                 style: TextStyle(color: Colors.grey),
                               )
                             ],
