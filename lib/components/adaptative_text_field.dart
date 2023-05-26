573bee709ce129f3a3ca13808ab45238b00c2846
Lucas Muniz Lara <lucasml@live.com>
2023-05-26 12:22:40 -0300

    adaptative form textfield

--- lib/components/adaptative_text_field.dart
+++ lib/components/adaptative_text_field.dart
@@ -3,29 +3,32 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 class AdaptativeTextField extends StatelessWidget {
-  final TextEditingController controler;
+  final TextEditingController controller;
   final TextInputType keyboardType;
   final Function(String) onSubmitted;
   final String label;
 
   const AdaptativeTextField(
       {super.key,
-      required this.controler,
-      required this.keyboardType,
+      required this.controller,
+      this.keyboardType = TextInputType.text,
       required this.onSubmitted,
       required this.label});
 
   @override
   Widget build(BuildContext context) {
     return Platform.isIOS
-        ? CupertinoTextField(
-            controller: controler,
-            keyboardType: keyboardType,
-            onSubmitted: onSubmitted,
-            placeholder: label,
+        ? Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: CupertinoTextField(
+              controller: controller,
+              keyboardType: keyboardType,
+              onSubmitted: onSubmitted,
+              placeholder: label,
+            ),
           )
         : TextField(
-            controller: controler,
+            controller: controller,
             keyboardType: keyboardType,
             onSubmitted: onSubmitted,
             decoration: InputDecoration(labelText: label),
